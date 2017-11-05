341b823ae2412bc69a22295479b7183e9ecce1d3
bflorin97 <bardasflorin97@gmail.com>
2017-11-05 00:13:27 +0000

    O implementare in O(n^2) pentru tema1-APD si am adaugat laboratoarele 4,5 RL

--- APD/Resurse Tema 1-20171030/parallel_snake.c
+++ APD/Resurse Tema 1-20171030/parallel_snake.c
@@ -1,9 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int index_in_bounds(int x, int dx, int limit) {
 	return x + dx >= limit ? x + dx - limit :
 	x + dx < 0 ? x + dx + limit : x + dx;
+}
+
+void print_matrix(int** world, int num_lines, int num_cols) {
+	for (int ii = 0; ii < num_lines; ii++) {
+		for (int jj = 0; jj < num_cols; jj++) {
+			printf("%2d ", world[ii][jj]);
+		}
+		printf("\n");
+	}
 }
 
 /* decodifica un intreg intr-o directie :
@@ -12,6 +22,7 @@ int index_in_bounds(int x, int dx, int limit) {
 2 - 'V'
 3 - 'E'
 */
+
 char itoc(int d) {
 	return d == 0 ? 'N' :
 	d == 1 ? 'S' :
@@ -22,11 +33,6 @@ char itoc(int d) {
 void run_simulation(int num_lines, int num_cols, int **world, int num_snakes,
 	struct snake *snakes, int step_count, char *file_name)
 {
-	// TODO: Implement Parallel Snake simulation using the default (env. OMP_NUM_THREADS)
-	// number of threads.
-	//
-	// DO NOT include any I/O stuff here, but make sure that world and snakes
-	// parameters are updated as required for the final state.
 
 	int step, k;
 	int gameOver = 0;
@@ -34,20 +40,92 @@ void run_simulation(int num_lines, int num_cols, int **world, int num_snakes,
 	char lastDirection = ' ';
 	char direction;
 	int i, j;
-
 	int y, x;
 	int di, dj;
 	int d;
 
-	// omp_set_num_threads(4);
+	struct snake* tails = (struct snake *)malloc(num_snakes * sizeof(struct snake));
 
-	for (step = 0; step < step_count && !gameOver; step++) {
+	/******************************************************
+	   I go throw every snake to find out the position
+	   of it's tail and save it in tails
+	******************************************************/
+	for (int k = 0; k < num_snakes; k++) {
+		snakeEnd = 0;
+		direction = snakes[k].direction;
 
-		/* --- check for collisions ---
-			Pot aparea coliziuni intre threaduri deoarece doua thread-uri pot
-			sa modifice si verifice simultan in acelasi loc din matrice.
-		*/
+		i = snakes[k].head.line;
+		j = snakes[k].head.col;
 
+		direction = snakes[k].direction;
+		i = snakes[k].head.line;
+		j = snakes[k].head.col;
+
+		di = direction == 'N' ? -1 : direction == 'S' ? 1 : 0;
+		dj = direction == 'V' ? -1 : direction == 'E' ? 1 : 0;
+
+		y = index_in_bounds(i, di, num_lines);
+		x = index_in_bounds(j, dj, num_cols);
+
+		lastDirection = direction;
+
+		while (!snakeEnd) {
+
+			di = dj = -1;
+			for (d = 0; d < 4; d++) {
+				direction = itoc(d);
+
+				if (direction == lastDirection) {
+					continue;
+				}
+
+				di = direction == 'N' ? -1 : direction == 'S' ? 1 : 0;
+				dj = direction == 'V' ? -1 : direction == 'E' ? 1 : 0;
+
+				y = index_in_bounds(i, di, num_lines);
+				x = index_in_bounds(j, dj, num_cols);
+
+				if (world[y][x] == snakes[k].encoding) {
+					break;
+				}
+			}
+
+			snakeEnd = d == 4;
+
+			if (snakeEnd) {
+				tails[k].head.line = i;
+				tails[k].head.col = j;
+				break;
+			}
+
+			i = index_in_bounds(i, di, num_lines);
+			j = index_in_bounds(j, dj, num_cols);
+
+			lastDirection = direction == 'N' ? 'S' :
+				direction == 'S' ? 'N' :
+				direction == 'E' ? 'V' :
+				direction == 'V' ? 'E' : ' ';
+		}
+	}
+
+
+	/******************************************************
+		Move snakes acording to direction
+	******************************************************/
+	for (step = 0; step < step_count; step++) {
+
+		/******************************************************
+			TODO
+			as putea pur si simplu sa consider capul sarpelui
+			cu un pas in fata, iar la faza de update sa mut
+			efectiv sarpele la momentul urmator (pentru a
+			scapa de '-1' care ramane in matrice)
+		******************************************************/
+
+
+		/******************************************************
+		Check collisions for each snake
+		******************************************************/
 		int v = num_snakes;
 		// #pragma omp parallel for num_threads(4)
 		for (k = 0; k < v; k++) {
@@ -70,46 +148,33 @@ void run_simulation(int num_lines, int num_cols, int **world, int num_snakes,
 
 			// #pragma omp critical
 			// {
-				if (world[y][x] != 0) {
-					gameOver = 1;
-					// break;
-					v = 0;
-				} else {
-					world[y][x] = -1;
-				}
+			if (world[y][x] != 0) {
+				gameOver = 1;
+				break;
+				v = 0;
+			} else {
+				world[y][x] = -1;
+			}
 			// }
 		}
 
-		// for (int ii = 0; ii < num_lines; ii++) {
-		// 	for (int jj = 0; jj < num_cols; jj++) {
-		// 		printf("%2d ", world[ii][jj]);
-		// 	}
-		// 	printf("\n");
-		// }
-		//
-		// printf("gameOver:[%d]; step:[%d]\n", gameOver, step);
-		// scanf("%c", &direction);
 
-		/*  --- update snakes ---
-		 	Este garantat ca nu exista coliziune cu alt sarpe sau cu el
-			insusi => mai multe thread-uri nu vor incerca sa acceseze
-			aceleasi resurse.
-		*/
+		/******************************************************
+			A collision was detected at step k+1 and it stoped
+			here to save matrix from step k
+		******************************************************/
 		if (gameOver) {
-			continue;
+			break;
 		}
 
-		for (int k = 0; k < num_snakes; k++) {
-			snakeEnd = 0;
-			lastDirection = ' ';
-			direction = snakes[k].direction;
+		/******************************************************
+			Update each snake, moving 1 step forward
+		******************************************************/
+		for (k = 0; k < num_snakes; k++) {
 
 			i = snakes[k].head.line;
 			j = snakes[k].head.col;
-
 			direction = snakes[k].direction;
-			i = snakes[k].head.line;
-			j = snakes[k].head.col;
 
 			di = direction == 'N' ? -1 : direction == 'S' ? 1 : 0;
 			dj = direction == 'V' ? -1 : direction == 'E' ? 1 : 0;
@@ -117,69 +182,39 @@ void run_simulation(int num_lines, int num_cols, int **world, int num_snakes,
 			y = index_in_bounds(i, di, num_lines);
 			x = index_in_bounds(j, dj, num_cols);
 
+			world[y][x] = snakes[k].encoding;
 			snakes[k].head.line = y;
 			snakes[k].head.col = x;
 
-			world[i][j] = 0;
-			world[y][x] = snakes[k].encoding;
-			lastDirection = direction;
+			i = tails[k].head.line;
+			j = tails[k].head.col;
+			for (d = 0; d < 4; d++) {
+				direction = itoc(d);
 
-			while (!snakeEnd) {
+				di = direction == 'N' ? -1 : direction == 'S' ? 1 : 0;
+				dj = direction == 'V' ? -1 : direction == 'E' ? 1 : 0;
 
-				di = dj = -1;
-				for (d = 0; d < 4; d++) {
-					direction = itoc(d);
+				y = index_in_bounds(i, di, num_lines);
+				x = index_in_bounds(j, dj, num_cols);
 
-					if (direction == lastDirection) {
-						continue;
-					}
-
-					di = direction == 'N' ? -1 : direction == 'S' ? 1 : 0;
-					dj = direction == 'V' ? -1 : direction == 'E' ? 1 : 0;
-
-					y = index_in_bounds(i, di, num_lines);
-					x = index_in_bounds(j, dj, num_cols);
-
-					if (world[y][x] == snakes[k].encoding) {
-						break;
-					}
-				}
-
-				snakeEnd = d == 4;
-
-				if (snakeEnd) {
-					world[i][j] = 0;
+				if (world[y][x] == snakes[k].encoding) {
 					break;
 				}
-
-				world[i][j] = snakes[k].encoding;
-				world[y][x] = 0;
-
-				i = index_in_bounds(i, di, num_lines);
-				j = index_in_bounds(j, dj, num_cols);
-
-				lastDirection = direction == 'N' ? 'S' :
-					direction == 'S' ? 'N' :
-					direction == 'E' ? 'V' :
-					direction == 'V' ? 'E' : ' ';
 			}
+			world[i][j] = 0;
+			tails[k].head.line = y;
+			tails[k].head.col = x;
 		}
 	}
 
+	/******************************************************
+		Clean up '-1' from matrix
+	******************************************************/
 	for (i = 0; i < num_lines; i++) {
-		for (j = 0; j < num_lines; j++) {
+		for (j = 0; j < num_cols; j++) {
 			if (world[i][j] == -1) {
 				world[i][j] = 0;
 			}
 		}
 	}
-	// for (int ii = 0; ii < num_lines; ii++) {
-	// 	for (int jj = 0; jj < num_cols; jj++) {
-	// 		printf("%d ", world[ii][jj]);
-	// 	}
-	// 	printf("\n");
-	// }
-	//
-	// printf("gameOver:[%d]; step:[%d]\n", gameOver, step);
-	printf("steps:[%d]\n", step);
 }
